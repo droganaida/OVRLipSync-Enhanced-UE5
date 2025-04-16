e836877101c8132f16746041593bae29f3c288ed
Aida Drogan <aida.drogan.box@gmail.com>
2025-04-16 10:56:44 -0300

    Improved viseme filtering and interpolation logic
    - Introduced FVisemeInterpolationSettings struct to centralize lip sync parameters.
    - Added support for skipping interpolation on consonant phonemes (bStrictConsonantLock).
    - Implemented filtering of short-duration visemes below MinHoldFrames to reduce visual noise.
    - Interpolation is now applied only after filtering low-importance frames.
    - All parameters are configurable via Blueprint or C++.

--- Source/OVRLipSync/Public/CookFrameSequenceAsync.h
+++ Source/OVRLipSync/Public/CookFrameSequenceAsync.h
@@ -1,4 +1,5 @@
-// Frame Sequence Maker fork by Aida Drogan based on CookFrameSequenceAsync by IlgarLunin https://github.com/IlgarLunin/UE4OVRLipSyncCookFrameSequence
+// Frame Sequence Maker fork by Aida Drogan based on CookFrameSequenceAsync by IlgarLunin
+// https://github.com/IlgarLunin/UE4OVRLipSyncCookFrameSequence
 
 #pragma once
 
@@ -10,6 +11,25 @@
 
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FFrameSequenceCoocked, UOVRLipSyncFrameSequence *, FrameSequence, bool,
 											 Success);
+
+USTRUCT(BlueprintType)
+struct FVisemeInterpolationSettings
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bEnableInterpolation = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	int32 MaxInterpolationFrames = 6;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool bStrictConsonantLock = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "LipSync", meta = (ClampMin = "1"))
+	int32 MinHoldFrames = 2;
+};
+
 /**
  * Generates Frame Sequence for LipSync
  */
@@ -19,19 +39,16 @@ class OVRLIPSYNC_API UCookFrameSequenceAsync : public UBlueprintAsyncActionBase
 	GENERATED_BODY()
 public:
 	UPROPERTY(BlueprintAssignable, Category = "LipSync")
-
 	FFrameSequenceCoocked onFrameSequenceCooked;
 
 	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true"), Category = "LipSync")
-	static UCookFrameSequenceAsync *CookFrameSequence(const TArray<uint8> &RawSamples, bool UseOfflineModel = false,
-													  bool bEnableInterpolation = true,
-													  int MaxInterpolationFrames = 12);
+	static UCookFrameSequenceAsync *
+	CookFrameSequence(const TArray<uint8> &RawSamples, bool UseOfflineModel = false,
+					  const FVisemeInterpolationSettings &Settings = FVisemeInterpolationSettings());
 
 	TArray<uint8> RawSamples;
 	bool UseOfflineModel;
-	bool bEnableInterpolation;
-	int MaxInterpolationFrames;
+	FVisemeInterpolationSettings InterpolationSettings;
 
 	virtual void Activate() override;
-	
 };
